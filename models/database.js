2e359a720c92bc608c93ec9effe8621509431de4
mayority <mayority11@gmail.com>
2020-09-17 09:26:35 +0100

    change stuff

--- models/database.js
+++ models/database.js
@@ -1,12 +1,10 @@
+require("dotenv").config();
 const db = require("mongoose");
-const connection = db.connect(
-  "mongodb+srv://mayorityz:majormayor@ifarms-app-wpmr0.mongodb.net/test?retryWrites=true&w=majority",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-  }
-);
+const connection = db.connect(process.env.MONGODBURL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
 
 module.exports = connection;
 //"mongodb+srv://mayorityz:majormayor@ifarms-app-wpmr0.mongodb.net/test?retryWrites=true&w=majority"
